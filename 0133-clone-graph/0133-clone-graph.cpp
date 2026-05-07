4d71d1728436699705d6bf4affdfd6618cfa96d9
Praveen <praveenc2212@gmail.com>
2026-05-07 19:04:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node== nullptr) return node;
+        queue<Node*> q ;
+        map<Node*,Node*> mp;
+
+        mp[node] = new Node(node->val);
+
+        q.push(node);
+        while(!q.empty() ){
+            Node* u = q.front();
+            q.pop();
+
+            for(Node* v : u->neighbors){
+                if(mp.find(v) == mp.end()) {   
+                    mp[v] = new Node(v->val);
+                    q.push(v);
+                }
+
+                mp[u]->neighbors.push_back( mp[v] );
+            }
+        }
+        return mp[node];
+    }
+};
