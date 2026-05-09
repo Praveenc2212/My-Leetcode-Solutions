c4cec4a8ae95d9946a60d2aef07a0b96a6ce16ae
Praveen <praveenc2212@gmail.com>
2026-05-09 21:40:07 +0530

    Time: 165 ms (51.46%), Space: 177.5 MB (31.14%) - LeetHub

---
+++ 3924-minimum-threshold-path-with-limited-heavy-edges/3924-minimum-threshold-path-with-limited-heavy-edges.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+
+    int n ; 
+    vector<vector<pair<int,int>>> adj;
+
+    bool Check(int mid , int src ,int target , int k  ){
+
+        deque<int> dq ;
+        vector<int> vis(n , 0 );
+        vis[src] = 1 ;
+        dq.push_front( src);
+        vector<int> dist(n, 1e9);
+        dist[src] = 0 ;
+
+        while( !dq.empty()){
+            int u = dq.front();
+
+            dq.pop_front();
+
+            for( auto& [v , w ] : adj[u]){
+                vis[v] = 1 ;
+                if( dist[v] > dist[u] + ( w > mid) ){
+                    dist[v] = dist[u] + ( w > mid);
+                    if( w > mid ) {
+                        dq.push_back( v );
+                    }
+                    else
+                        dq.push_front( v );
+                }
+            }
+        }
+        return dist[target] <= k ;
+    }
+    
+    int minimumThreshold(int nn, vector<vector<int>>& edges, int src, int target, int k) 
+        {
+        adj.clear();
+
+        // if( src == target) return 0;
+        
+        n = nn;
+        int low = 0 , high = 1 + 1e9;
+        adj.resize(n);
+
+        for( auto e : edges ){
+            int u =e[0], v = e[1] , w =e[2];
+            adj[u].push_back({v , w });
+            adj[v].push_back({u, w });
+        }
+
+
+        int ans = -1 ;
+        while( low <= high ){
+            int m = low + ( high - low ) / 2 ;
+            if(Check(m , src , target , k )){
+                ans = m;
+                high = m -1 ;
+            }
+            else low = m + 1 ;
+        }
+        
+        return ans;
+        
+    }
+};
