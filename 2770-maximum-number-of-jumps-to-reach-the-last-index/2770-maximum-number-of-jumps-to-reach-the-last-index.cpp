d0281869da09a394b3eca6c0ff64fa35904f16b3
Praveen <praveenc2212@gmail.com>
2026-05-11 18:21:17 +0530

    Time: 14 ms (83.02%), Space: 67.5 MB (92.59%) - LeetHub

---
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int n ;
+    long long dp[1005];
+    long long fun( int i , int target , vector<int>&nums ){
+
+        if( i == n-1 ){
+            return   0;
+        }
+        if( dp[i] != -1 ) return dp[i];
+        long long ans = -1e18 ;
+        
+        for( int j = i + 1 ;j < n ; j += 1  ){
+            int diff = nums[j] - nums[i] ;
+            if( -target <= diff && diff <= target ){
+                ans = max( ans , fun( j , target , nums  ) + 1 );
+            }
+        }
+        return dp[i] =  ans;
+    }
+    int maximumJumps(vector<int>& nums, int target) 
+    {
+        n = nums.size();
+        memset( dp , -1 , sizeof( dp) ) ;
+        long long res = fun( 0 , target ,nums  );
+        return res < 0 ? -1 : res ;
+    }
+};
