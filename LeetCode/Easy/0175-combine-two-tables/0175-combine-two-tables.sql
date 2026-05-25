c984c6b07663977ed3f72ebbe2b4f34200083643
Praveen <praveenc2212@gmail.com>
2026-05-25 17:44:42 +0530

    0175-combine-two-tables Easy .sql 405 ms (66.95%) Database 05-25-2026

---
+++ LeetCode/Easy/0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,10 @@
+
+
+
+select p.firstName as firstName , 
+        p.lastName as lastName ,
+        a.city as city ,
+        a.state as state
+from person p
+left join address a 
+ on p.personId = a.personId;
